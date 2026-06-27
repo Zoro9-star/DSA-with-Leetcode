dc23fc0c84d8f417808bd132dc739fd2bacd8fb3
Zoro9-star <pradhannanda547@gmail.com>
2026-06-27 21:22:30 +0530

    Added Kadane's Algorithm and LC QuesNo 53 & 152

--- Kadane Algorithm/LeetCode_152.py
+++ Kadane Algorithm/LeetCode_152.py
@@ -1,20 +1,26 @@
-# Ques No 53 - Maximum Subarray
+# Ques No 152 - Maximum Product Subarray
 
 # Problem Statement:
-# Given an integer array nums, find the with the largest sum, and return its sum.
+# Given an integer array nums, find a that has the largest product, and return the product.
 
+# The test cases are generated so that the answer will fit in a 32-bit integer.
+
+# Note that the product of an array with a single element is the value of that element.
+
+ 
 
 # Example 1:
 
-# Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+# Input: nums = [2,3,-2,4]
 # Output: 6
-# Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+# Explanation: [2,3] has the largest product 6.
 
 # Example 2:
 
-# Input: nums = [1]
-# Output: 1
-# Explanation: The subarray [1] has the largest sum 1.
+# Input: nums = [-2,0,-1]
+# Output: 0
+# Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+
 
 
 class Solutionn:
