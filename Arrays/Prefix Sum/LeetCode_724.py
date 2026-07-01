1668645b51b3eeb386c205b99eee512a18f47ed1
Zoro9-star <pradhannanda547@gmail.com>
2026-07-01 18:07:33 +0530

    Added Prefix Sum and QuesNo 724 - Find Pivot Point

---
+++ Arrays/Prefix Sum/LeetCode_724.py
@@ -0,0 +1,44 @@
+# Ques No 724 - Find Pivot Point
+
+# Problem Statement:
+# Given an array of integers nums, calculate the pivot index of this array.
+
+# The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+
+# If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+
+# Return the leftmost pivot index. If no such index exists, return -1.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,7,3,6,5,6]
+# Output: 3
+# Explanation:
+# The pivot index is 3.
+# Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+# Right sum = nums[4] + nums[5] = 5 + 6 = 11
+
+# Example 2:
+
+# Input: nums = [1,2,3]
+# Output: -1
+# Explanation:
+# There is no index that satisfies the conditions in the problem statement.
+
+
+
+class Solution:
+    def PivotPoint(self , nums):
+        prefix = 0
+        suffix = 0
+        for i in range(len(nums)):
+            suffix = sum(nums)-prefix - nums[i]
+
+            if prefix == suffix:
+                return i 
+            prefix += nums[i]
+        return -1
+obj = Solution()
+print(obj.PivotPoint([1,7,3,6,5,6]))
